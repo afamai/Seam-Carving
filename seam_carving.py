dffe16c9171db3d4966919bc5f1b299dd08b2f78
afamai <alpha.mai@ryerson.ca>
2020-09-17 03:15:02 -0400

    modularize code into functions

--- seam_carving.py
+++ seam_carving.py
@@ -5,57 +5,56 @@ from scipy.ndimage import correlate
 import numpy as np
 import time
 
+def compute_scoring_matrix(image):
+    # computer the energy image using gradient magnitude
+    matrix = filters.sobel(color.rgb2gray(image))
+
+    r, c = matrix.shape
+    for i in range(1, r):
+        for j in range(c):
+            if (j == 0):
+                matrix[i, j] += min(matrix[i-1, j], matrix[i-1, j+1])
+            elif (j == c-1):
+                matrix[i, j] += min(matrix[i-1, j-1], matrix[i-1, j])
+            else:
+                matrix[i, j] += min(matrix[i-1, j-1], matrix[i-1, j], matrix[i-1, j+1])
+    
+    return matrix
+
+def minimum_seam(matrix):
+    r, c = matrix.shape
+    # mask = np.ones((r, c), dtype=np.bool)
+    mask = []
+    for i in reversed(range(r)):
+        if i == r-1:
+            j = np.argmin(matrix[-1])
+        else:
+            if j == 0:
+                j += np.argmin(matrix[i, j:j+2])
+            else:
+                j += np.argmin(matrix[i, j-1:j+2]) - 1
+        # mask[i, j] = False
+        mask.append(i * c + j)
+    return mask
+
+def remove_seams(image, amount):
+    img = image.copy()
+    for _ in range(amount):
+        score_matrix = compute_scoring_matrix(img)
+        mask = minimum_seam(score_matrix)
+
+        # remove seam from image
+        r, c, _ = img.shape
+        # img = img[mask].reshape((r, c-1, 3))
+        img = np.delete(img.reshape((r*c, 3)), mask, axis=0).reshape((r, c-1, 3))
+    
+    return img
 
 @click.command()
 def cli():
-    click.echo("hello world")
-    img = io.imread("example02.jpg")
-    original = img.copy()
-    record = []
-    start = time.time() 
-    for n in range(100):
-        energy = filters.sobel(color.rgb2gray(img))
-        matrix = energy.copy()
-        r, c = matrix.shape
-        for i in range(1, r):
-            for j in range(c):
-                if (j == 0):
-                    matrix[i, j] += min(matrix[i-1, j], matrix[i-1, j+1])
-                elif (j == c-1):
-                    matrix[i, j] += min(matrix[i-1, j-1], matrix[i-1, j])
-                else:
-                    matrix[i, j] += min(matrix[i-1, j-1], matrix[i-1, j], matrix[i-1, j+1])
-
-        # remove seam
-        seam = []
-        for i in reversed(range(r)):
-            if i == r-1:
-                j = np.argmin(matrix[-1])
-            else:
-                if j == 0:
-                    j += np.argmin(matrix[i, j:j+2])
-                else:
-                    j += np.argmin(matrix[i, j-1:j+2]) - 1
-
-            seam.append(i * c + j)
-
-        img = np.delete(img.reshape((r*c, 3)), seam, axis=0)
-        img = img.reshape((r, c-1, 3))
-        record.append(seam)
-        print(str(n) + " seam complete")
-    
-    click.echo("Time taken: " + str(time.time() - start))
-
-    seam_removed = img.reshape((-1,3))
-    for seam in reversed(record):
-        for idx in reversed(seam):
-            seam_removed = np.insert(seam_removed, idx, [255,0,0], axis=0)
-
-    seam_removed = seam_removed.reshape(original.shape)
-                
-    plt.imshow(img)
+    image = io.imread("example01.jpg")
+    start = time.time()
+    im = remove_seams(image, 30)
+    print("Time: " + str(time.time() - start))
+    plt.imshow(im)
     plt.show()
-
-    plt.imshow(seam_removed)
-    plt.show()
-    
